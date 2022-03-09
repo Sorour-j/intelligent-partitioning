1d231f2ede5ca6883181ee5c3dfe33a522d70ba6
Sorour <sorour.jahanbin@york.ac.uk>
2022-03-09 12:36:17 +0000

    Added Simple Example

---
+++ org.eclipse.epsilon.partialloading/src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/PartialXMIResourceTest2.java
@@ -0,0 +1,81 @@
+package org.eclipse.epsilon.effectivemetamodel.example.Standalone;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.XMLLoad;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.epsilon.emc.emf.xmi.PartialXMIHelper;
+import org.eclipse.epsilon.emc.emf.xmi.PartialXMILoadConfiguration;
+import org.eclipse.epsilon.emc.emf.xmi.PartialXMIResourceFactory;
+
+public class PartialXMIResourceTest2 {
+	
+	public static final String OPTION_PARTIAL_LOADING_CONFIGURATION = "PARTIAL_LOADING_CONFIGURATION";
+	public static ResourceSet ecoreResourceSet = new ResourceSetImpl();
+	public static ResourceSet xmiResourceSet = new ResourceSetImpl();
+	public static Resource resource;
+	public static String metamodel ="src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/IMDB.ecore";
+	protected PartialXMILoadConfiguration configuration;
+	static String uri = "imdb";
+	//static String uri = "http://movies/1.0";
+
+	public static void main(String[] args) throws Exception {
+		
+		RegisterEcore(metamodel);
+		
+		xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new PartialXMIResourceFactory());
+		XMIResource resource = (XMIResource) xmiResourceSet.createResource(URI.createFileURI("src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/imdb.xmi"));
+		PartialXMILoadConfiguration configuration = new PartialXMILoadConfiguration();
+
+		EClass eclass1 = (EClass)xmiResourceSet.getPackageRegistry().getEPackage(uri).getEClassifiers().get(1);
+		EStructuralFeature feature1 = eclass1.getEAllReferences().get(0);
+		configuration.addAllOfKind(eclass1); //Movie
+		configuration.addFeature(eclass1, feature1); //Movie -> persons: Person
+		
+		EClass eclass2 = (EClass)xmiResourceSet.getPackageRegistry().getEPackage(uri).getEClassifiers().get(2);//Person
+		EStructuralFeature feature2 = eclass2.getEAllAttributes().get(0);
+		configuration.addFeature(eclass2, feature2); //Person, name
+
+		HashMap<String, Object> loadOptions = new HashMap<>();
+		loadOptions.put(OPTION_PARTIAL_LOADING_CONFIGURATION, configuration);
+		resource.load(loadOptions);
+		
+		resource.save(System.out, null); 
+	}
+	
+	static void RegisterEcore(String metamodel) {
+		
+		ecoreResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+		Resource ecoreResource = ecoreResourceSet
+				.createResource(URI.createFileURI(new File(metamodel).getAbsolutePath()));
+		try {
+			ecoreResource.load(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		for (EObject o : ecoreResource.getContents()) {
+			EPackage ePackage = (EPackage) o;
+			xmiResourceSet.getPackageRegistry().put(ePackage.getNsURI(), ePackage);
+		}
+		System.out.println("Registeration Done!......");
+	}
+}
---
+++ org.eclipse.epsilon.partialloading/src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/ProduceXmiID.java
@@ -0,0 +1,72 @@
+package org.eclipse.epsilon.effectivemetamodel.example.Standalone;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+public class ProduceXmiID {
+
+	public static ResourceSet ecoreResourceSet = new ResourceSetImpl();
+	public static ResourceSet xmiResourceSet = new ResourceSetImpl();
+	public static XMIResource resource;
+	static String uri = "http://www.eclipse.org/MoDisco/Java/0.2.incubation/java";
+	public static void main(String[] args) throws Exception {
+		 
+	ecoreResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+	Resource ecoreResource = ecoreResourceSet
+			.createResource(URI.createFileURI(new File("src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/java.ecore").getAbsolutePath()));
+	try {
+		ecoreResource.load(null);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl() {
+		@Override
+		public XMIResourceImpl createResource(URI uri) {
+			return new XMIResourceImpl(uri) {
+				@Override
+				protected boolean useUUIDs() {
+					return true;
+				}
+			};
+		}
+	});
+	
+	//xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+	for (EObject o : ecoreResource.getContents()) {
+		EPackage ePackage = (EPackage) o;
+		xmiResourceSet.getPackageRegistry().put(ePackage.getNsURI(), ePackage);
+	}
+	System.out.println("Registeration Done!......");
+
+	resource = (XMIResource) xmiResourceSet.createResource(URI.createFileURI("src/org/eclipse/epsilon/effectivemetamodel/example/Standalone/eclipseModel-0.1.2.xmi"));
+	resource.load(null);
+	
+	
+//	resourceWithIDs.getContents().addAll(EcoreUtil.copyAll(resource.getContents()));
+	
+	final Map<Object, Object> saveOptions = resource.getDefaultSaveOptions();
+	saveOptions.put(XMIResource.OPTION_SCHEMA_LOCATION,Boolean.TRUE);
+	saveOptions.put(XMIResource.OPTION_ENCODING,"UTF-8");
+	saveOptions.put(XMIResource.OPTION_USE_XMI_TYPE, Boolean.TRUE);
+	saveOptions.put(XMIResource.OPTION_SAVE_TYPE_INFORMATION,Boolean.TRUE);
+	
+	//resource.save(System.out,saveOptions);
+	resource.save(saveOptions);
+	
+}
+}
